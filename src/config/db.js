64a4d88cbe44a6659c5c3c02c22aa595f04acf47
Miguel Lopes <migueldollopes@gmail.com>
2026-04-29 15:33:53 -0300

    feat/consolidação da api e testes finais

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,14 @@
 import fs from 'fs/promises'
 
 async function readGames() {
-    const data = await fs.readFile('../data/games.json', 'utf-8');
+    const data = await fs.readFile('./src/data/games.json', 'utf-8');
     const games = JSON.parse(data);
     return games;
 }
 
 async function writeGames(games) {
     const data = JSON.stringify(games, null, 2)
-    await fs.writeFile('./data/game.json', data, 'utf-8')
+    await fs.writeFile('./src/data/games.json', data, 'utf-8')
 }
+
+export default readGames 
--- src/service/gamesService.js
+++ src/service/gamesService.js
@@ -1,17 +1,17 @@
-import { readArtists } from '../config/db.js'
- 
+import readGames from '../config/db.js'
+
 class GamesServices {
     async getAll() {
         try {
-            const games = await readArtists()
-            return artists
+            const games = await readGames()
+            return games
         } catch (error) {
             console.log(error);
         }
     }
 
     async getById(id) {
-        const games = await readArtists()
+        const games = await readGames()
         const game = games.find(g => g.id === Number(id))
         return game
     }
