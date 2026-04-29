2634a9d0538641df546ed792adebee08640966bc
Miguel Lopes <migueldollopes@gmail.com>
2026-04-29 14:45:18 -0300

    feat/anexando rotas e fixando services

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-import express from express
+import express from 'express'
+import route from './routes/gamesRoutes.js'
 const app = express()
 const PORT = 3000
 app.use(express.json())
@@ -6,6 +7,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('API de Jogos desenvolvida em grupo, por: Henrique Hoppen, Miguel Lopes, Eduardo Casanova e Fernando Zhang.')
 })
+
+app.use('/games', route)
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`)
--- src/service/gamesService.js
+++ src/service/gamesService.js
@@ -1,54 +1,20 @@
-import games from '../data/games.json' assert { type: 'json' }
+import { readArtists } from '../config/db.js'
  
-// Da uma bisolhada no JSON e retorna como um array
-async function getAllGames() {
-    return games
-}
- 
-// Mostra um jogo específico com o id dele
-async function getGameById(id) {
-    const game = games.find(g => g.id === Number(id))
-    return game || null
-}
- 
-// Cria um novo gameles e salva no JSON
-async function createGame(newGame) {
-    const lastId = games.length > 0 ? games[games.length - 1].id : 0
-    const game = {
-        id: lastId + 1,
-        game: newGame.game,
-        publisher: newGame.publisher,
-        year: newGame.year
+class GamesServices {
+    async getAll() {
+        try {
+            const games = await readArtists()
+            return artists
+        } catch (error) {
+            console.log(error);
+        }
     }
- 
-    games.push(game)
-    return game
-}
- 
-// Atualiza um gameles com o id dele no JSON
-async function updateGame(id, updatedData) {
-    const index = games.findIndex(g => g.id === Number(id))
- 
-    if (index === -1) return null
- 
-    games[index] = { ...games[index], ...updatedData, id: games[index].id }
-    return games[index]
-}
- 
-// Evapora o gameles com o id dele no JSON
-async function deleteGame(id) {
-    const index = games.findIndex(g => g.id === Number(id))
- 
-    if (index === -1) return null
- 
-    const deleted = games.splice(index, 1)
-    return deleted[0]
-}
- 
-export {
-    getAllGames,
-    getGameById,
-    createGame,
-    updateGame,
-    deleteGame
-}
+
+    async getById(id) {
+        const games = await readArtists()
+        const game = games.find(g => g.id === Number(id))
+        return game
+    }
+};
+
+export const gameServices = new GamesServices();
